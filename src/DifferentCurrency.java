2ae79941d1e3cd9d17bcf566d386be6bcc8d95ff
ttnan <ttnana882@gmail.com>
2023-08-01 20:31:16 -0400

    Added three new countries' currencies. Added rates for currencies and oldrate variable

--- src/DifferentCurrency.java
+++ src/DifferentCurrency.java
@@ -14,18 +14,30 @@ public class DifferentCurrency
     private static Currency c2 = Currency.getInstance("JPY");
 	private static Currency c3 = Currency.getInstance("USD");
 	private static Currency c1 = Currency.getInstance(Locale.FRANCE);
+	private static Currency c4 = Currency.getInstance(Locale.CHINA);
+	private static Currency c5 = Currency.getInstance(Locale.CANADA);
+	private static Currency c6 = Currency.getInstance(Locale.GERMANY);
 	//symbol
 	public static String sym1 = c1.getSymbol();
 	public static String sym2 = c2.getSymbol();
 	public static String sym3 = c3.getSymbol();
+	public static String sym4 = c4.getSymbol();
+	public static String sym5 = c5.getSymbol();
+	public static String sym6 = c6.getSymbol();
 	//Display name
     public static String cur2 = c2.getDisplayName();
     public static String cur1 = c1.getDisplayName();
     public static String cur3 = c3 .getDisplayName();
+    public static String cur4 = c4.getDisplayName();
+    public static String cur5 = c5.getDisplayName();
+    public static String cur6 = c6 .getDisplayName();
     //Exchange rates
     public static double rate1 = 0.91;
     public static double rate2 = 143.04;
     public static double rate3 = 1;
+    public static double rate4 = 7.18;
+    public static double rate5 = 1.33;
+    public static double rate6 = 0.91;
     
     
 
@@ -67,18 +79,37 @@ class DropDownCurrencyChange {
                 String selectedOption = (String) dropdown.getSelectedItem();
                 String sym = DifferentCurrency.sym1;
                 double rate = DifferentCurrency.rate1;
+                double oldrate = rate;
                 if (selectedOption.equals(DifferentCurrency.cur1)) {
+                	oldrate = rate;
                 	sym = DifferentCurrency.sym1;
                 	rate = DifferentCurrency.rate1;
-                    System.out.println("Option 1 selected. Performing action 1.");
+                    //System.out.println("Option 1 selected. Performing action 1.");
                 } else if (selectedOption.equals(DifferentCurrency.cur2)) {
+                	oldrate = rate;
                 	sym = DifferentCurrency.sym2;
                 	rate = DifferentCurrency.rate2;
-                    System.out.println("Option 2 selected. Performing action 2.");
+                    //System.out.println("Option 2 selected. Performing action 2.");
                 } else if (selectedOption.equals(DifferentCurrency.cur3)) {
+                	oldrate = rate;
                 	sym = DifferentCurrency.sym3;
                 	rate = DifferentCurrency.rate3;
-                    System.out.println("Option 3 selected. Performing action 3.");
+                   // System.out.println("Option 3 selected. Performing action 3.");
+                }
+                else if (selectedOption.equals(DifferentCurrency.cur4)) {
+                	oldrate = rate;
+                	sym = DifferentCurrency.sym4;
+                	rate = DifferentCurrency.rate4;
+                }
+                else if (selectedOption.equals(DifferentCurrency.cur5)) {
+                	oldrate = rate;
+                	sym = DifferentCurrency.sym5;
+                	rate = DifferentCurrency.rate5;
+                }
+                else if (selectedOption.equals(DifferentCurrency.cur6)) {
+                	oldrate = rate;
+                	sym = DifferentCurrency.sym6;
+                	rate = DifferentCurrency.rate6;
                 }
             }
 
