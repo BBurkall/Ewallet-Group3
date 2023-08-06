98e61d0d5c03fa3936b90d53ebc7e940f46b0fdb
AndyLewis15 <114292647+AndyLewis15@users.noreply.github.com>
2023-08-06 18:21:20 -0400

    Fixed issue with expense calculation.
    Need to fix issue with income now where it is counting total income for every month and not just the monthly income.

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -1007,7 +1007,22 @@ class estimatePanel extends JPanel {
         // Calculate the total monthly expenses
         double totalExpenses = 0;
         for (Expense expense : expenses) {
-            totalExpenses += (expense.getAmount() * expense.getFrequency());
+            double amount = expense.getAmount();
+            int yearlyFrequency = expense.getFrequency();
+
+            // Convert the expense amount based on its frequency
+            switch (yearlyFrequency) {
+                case 1: // One time or once a year
+                    totalExpenses += amount;
+                    break;
+                case 12: // Monthly
+                    totalExpenses += amount * 12;
+                    break;
+                case 24: // Biweekly
+                    totalExpenses += amount * 24;
+                    break;
+                default:
+            }
         }
 
         // Calculate the total monthly income
